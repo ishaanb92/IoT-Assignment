29a3807e31eedde3c672e186e94a72c836c35aba
kilgore92 <ishaan.bits92@gmail.com>
2017-11-30 22:08:25 +0100

    Updating hyper params. No improvements

--- nn.py
+++ nn.py
@@ -17,27 +17,35 @@ def load_data(fileName):
         data = pickle.load(f)
     return np.asarray(data)
 
+# Hyper-parameters
+epochs = 50
+batch_size = 256
+learning_rate = 1e-5
+lmb = 0.5
 #Place holders for data
 x = tf.placeholder(tf.float64,[None,7])
 y = tf.placeholder(tf.float64,[None])
 #Hidden Layer 1
-Wh_1 = tf.Variable(tf.truncated_normal([7,4], mean=0.0, stddev=1.0, dtype=tf.float64),name = 'Wh_1')
-bh_1 = tf.Variable(tf.zeros(shape= [4],dtype=tf.float64),name = 'bh_1')
+Wh_1 = tf.Variable(tf.truncated_normal([7,5], mean=0.0, stddev=1.0, dtype=tf.float64),name = 'Wh_1')
+bh_1 = tf.Variable(tf.zeros(shape= [5],dtype=tf.float64),name = 'bh_1')
 out1 = tf.nn.relu(tf.add(tf.matmul(x,Wh_1),bh_1))
-#Out
-W_out = tf.Variable(tf.truncated_normal([4,1], mean=0.0, stddev=1.0, dtype=tf.float64),name = 'W_out')
+#Hidden Layer 2
+Wh_2 = tf.Variable(tf.truncated_normal([5,3], mean=0.0, stddev=1.0, dtype=tf.float64),name = 'Wh_1')
+bh_2 = tf.Variable(tf.zeros(shape= [3],dtype=tf.float64),name = 'bh_1')
+out2 = tf.nn.relu(tf.add(tf.matmul(out1,Wh_2),bh_2))
+#Output
+W_out = tf.Variable(tf.truncated_normal([3,1], mean=0.0, stddev=1.0, dtype=tf.float64),name = 'W_out')
 b_out = tf.Variable(tf.zeros(shape=[1],dtype=tf.float64),name = 'b_out')
-out = tf.add(tf.matmul(out1,W_out),b_out)
+out = tf.add(tf.matmul(out2,W_out),b_out)
+#Regularization
+regularizers = tf.nn.l2_loss(W_out) + tf.nn.l2_loss(Wh_1) + tf.nn.l2_loss(Wh_2)
 #MSE cost
-cost = tf.reduce_mean(tf.square(y-out))
+cost = tf.reduce_mean(tf.square(tf.subtract(y,out)))
+loss = tf.reduce_mean(cost + lmb*regularizers)
 #Train step
-learning_rate = 0.00025
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-epochs = 100
-batch_size = 64
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 init = tf.global_variables_initializer()
 params = tf.trainable_variables()
-print(params)
 print('Tunable parameters : {}'.format(len(params)))
 gradients = tf.gradients(cost, params)
 gradient_norm = tf.global_norm(gradients)
@@ -58,4 +66,6 @@ with tf.Session() as sess:
                 print('Epoch {} Step {} :: Gradient norm : {}'.format(ep,step,grad_norm))
                 print('Epoch {} Step {} :: Training cost : {}'.format(ep,step,sess.run([cost],feed_dict={x:batch_data,y:batch_labels})))
                 print('Epoch {} Step {} :: Validation cost: {}'.format(ep,step,sess.run([cost],feed_dict={x:X_val,y:labels_val})))
+                #print('Epoch {} Step {} :: Predicted : {} True {}'.format(ep,step,sess.run([out],feed_dict={x:batch_data,y:batch_labels}),batch_labels))
+
 
